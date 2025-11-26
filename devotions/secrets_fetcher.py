999c038188b8bdcd95d26a84f0b91cb0b8c05878
brandonadamprice <44103392+brandonadamprice@users.noreply.github.com>
2025-11-26 06:41:04 -0500

    Make cards collapsable and also addd a check for an ESV_API_KEY enviornment variable for local testing

--- devotions/secrets_fetcher.py
+++ devotions/secrets_fetcher.py
@@ -4,6 +4,8 @@ from google.cloud import secretmanager
 
 def get_esv_api_key():
   """Fetches the ESV API key from the secrets manager."""
+  if os.getenv("ESV_API_KEY") is not None:
+    return os.getenv("ESV_API_KEY")
   secret_id = "projects/978583660884/secrets/ESV_API_KEY/versions/1"
   secret_manager = secretmanager.SecretManagerServiceClient()
   response = secret_manager.access_secret_version(name=secret_id)
