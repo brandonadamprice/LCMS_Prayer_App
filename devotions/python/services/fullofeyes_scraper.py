da6323c5770a47387a334838699e39e0e740109b
Brandon <BrandonAdamPrice@gmail.com>
2026-01-13 19:58:05 +0000

    try a fix

--- devotions/python/services/fullofeyes_scraper.py
+++ devotions/python/services/fullofeyes_scraper.py
@@ -48,7 +48,6 @@ class FullOfEyesScraper:
             "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
         ),
         "Accept-Language": "en-US,en;q=0.9",
-        "Accept-Encoding": "gzip, deflate, br",
         "Referer": "https://www.google.com/",
         "Upgrade-Insecure-Requests": "1",
         "Sec-Fetch-Dest": "document",
