cd739c1d74c9045204d286c3f6f27fbb9dce434c
Brandon <BrandonAdamPrice@gmail.com>
2026-01-14 20:13:13 +0000

    update secrets fetcher

--- devotions/python/secrets_fetcher.py
+++ devotions/python/secrets_fetcher.py
@@ -9,18 +9,24 @@ _SECRET_VERSION = "1"
 
 def _get_secret(secret_name, environment_variable, version=_SECRET_VERSION):
   """Fetches a secret from environment variables or Google Cloud Secret Manager."""
+  val = None
   if os.getenv(environment_variable) is not None:
-    return os.getenv(environment_variable)
-  try:
-    secret_id = (
-        f"projects/{PROJECT_ID}/secrets/{secret_name}/versions/{version}"
-    )
-    client = secretmanager.SecretManagerServiceClient()
-    response = client.access_secret_version(name=secret_id)
-    return response.payload.data.decode("UTF-8")
-  except Exception as e:
-    print(f"Failed to fetch secret: {secret_name} - Error: {e}")
-    return None
+    val = os.getenv(environment_variable)
+  else:
+    try:
+      secret_id = (
+          f"projects/{PROJECT_ID}/secrets/{secret_name}/versions/{version}"
+      )
+      client = secretmanager.SecretManagerServiceClient()
+      response = client.access_secret_version(name=secret_id)
+      val = response.payload.data.decode("UTF-8")
+    except Exception as e:
+      print(f"Failed to fetch secret: {secret_name} - Error: {e}")
+      return None
+
+  if val:
+    return val.strip()
+  return val
 
 
 def get_esv_api_key():
